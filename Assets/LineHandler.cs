6b53824bfc194aa510b836982b36925994d087f4
Gabriele Zotti <zottigabriele@hotmail.com>
2020-08-04 11:25:07 +0200

    Init

---
+++ Assets/LineHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LineHandler : MonoBehaviour
+{
+    [SerializeField] TMP_InputField _inField;
+
+    public string cmd;
+    // public string[] args;
+
+    public void OnEndEdit(string cmd) {
+
+        if(cmd == "") { EventSystem.current.SetSelectedGameObject(_inField.gameObject); return; }
+
+        this.cmd = cmd;
+
+        // string[] cmd_strings = cmd.Split();
+
+        // this.cmd = cmd_strings[0];
+        // args = new string[cmd_strings.Length];
+        
+        // for (int i = 1; i < cmd_strings.Length; i++)
+        // {
+        //     args[i-1] = cmd_strings[i];
+        // }
+
+        _inField.readOnly = true;
+        _inField.interactable = false;
+        _inField.GetComponent<LayoutElement>().flexibleHeight = 0;
+
+        TerminalHandler.Instance.OnCommandInputEnd(this);
+    }
+}
---
+++ Assets/TerminalHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using System.Text.RegularExpressions;
+
+public class TerminalHandler : MonoBehaviour
+{
+    public static TerminalHandler Instance {
+        get; private set;
+    }
+
+    // [SerializeField] public LineCmdCollection;
+
+    void Awake()
+    {
+        if(!Instance) {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        } else if (Instance != this) {
+            Destroy(this);
+        }
+    }
+
+    [SerializeField] GameObject _lineTameplate;
+
+    public void OnCommandInputEnd(LineHandler line) {
+        // parseCommand(line.cmd, line.args);
+        parseCommand(line.cmd);
+        instantiateNewLine();
+    }
+
+    private void instantiateNewLine() {
+        var new_line = Instantiate(_lineTameplate, transform).GetComponentInChildren<TMP_InputField>();
+        EventSystem.current.SetSelectedGameObject(new_line.gameObject);
+    }
+
+    private void parseCommand(string cmd) {
+
+        Match m = Regex.Match(cmd, "^ *clear$");
+
+        if(m.Success) {
+            foreach (Transform child in transform)
+            {
+                if(child.tag != "Background") {
+                    Destroy(child.gameObject);
+                }
+            }
+        } else {
+            Debug.LogWarning("No cmd match for \"" + cmd + "\"");
+        }
+
+        // switch (cmd)
+        // {
+        //     case "clear":
+        //         foreach (Transform child in transform)
+        //         {
+        //             if(child.tag != "Background") {
+        //                 Destroy(child.gameObject);
+        //             }
+        //         }
+        //         break;
+        //     default: Debug.LogWarning("No cmd match for \"" + cmd + "\""); break;
+        // }
+    }
+}
